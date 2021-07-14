f4dc7d16263b6dcd2b7eadad67d88ef4502264b2
Sangwan Han <jklom0326@gmail.com>
2021-07-14 22:39:56 +0900

    bignerdranchguide

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -1,24 +1,64 @@
 package NyetHack
 
+import java.io.File
 import kotlin.math.roundToInt
 
 const val TAVENR_NAME = "Tearnyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val menuList = File("src/data/tavern-menu-items.txt")
+    .readText()
+    .split("\r\n")
 
 fun main() {
+    menuList.forEachIndexed { index, data ->
+        println("${index + 1}: $data")
+    }
+
 //    var baverage = readLine()
-//
+    // let 사용
 //    baverage?.let {
 //        baverage = it.capitalize()
 //        println(baverage)
 //    }?: println("beverage가 null입니다!")
-    placeOrder("shandy,Dragon's Breath,15.91")
 //    placeOrder("elixir,Shirley's Temple, 4.12")
+
+    // list 포함
+//    if (patronList.contains("Eli")){
+//        println("술집 주인이 말한다: Eli는 안쪽 방에서 카드하고 있어요.")
+//    } else{
+//        println("술집 주인이 말한다: Eli는 여기에 없어요")
+//    }
+//
+//    if (patronList.containsAll(listOf("Mordoc","Sophie"))) {
+//        println("술집 주인이 말한다: 네, 모두있어요")
+//    } else {
+//        println("술집 주인이 말한다: 아니오, 나간사람도 있습니다.")
+//    }
+
+    // list
+//    println(patronList)
+//    patronList.add("Alex")
+//    patronList.remove("Eli")
+//    patronList.add(0,"Alex")
+//    patronList[0] = "Alexis"
+//    println(patronList)
+    // list for 사용
+//    for (patron in patronList) {
+//        println("좋은 밤입니다. ${patron}님")
+//    }
+    // list forEachIndex
+    patronList.forEachIndexed { index, patron ->
+        println("좋은 밤입니다. ${patron}님 당신은 ${index + 1}번째 손님입니다")
+        placeOrder(patron, menuList.shuffled().first())
+    }
+
+
 }
 
-fun performPurchase(price : Double): Boolean {
+fun performPurchase(price: Double): Boolean {
     displayBalance()
     val totalPurse = playerGold + (playerSilver / 100.0)
     println("지갑 전체 금액: 금화 $totalPurse")
@@ -28,12 +68,12 @@ fun performPurchase(price : Double): Boolean {
         println("남은 잔액: ${"%.2f".format(remainingBalance)}")
 
         val remainingGold = remainingBalance.toInt()
-        val remainingSilver = (remainingBalance%1*100).roundToInt()
+        val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
         playerGold = remainingGold
         playerSilver = remainingSilver
         displayBalance()
     } else {
-     println("돈이 부족해!")
+        println("돈이 부족해!")
         return false
     }
     return true
@@ -43,27 +83,28 @@ fun displayBalance() {
     println("플레이어의 지갑 잔액: 금화: $playerGold 개, 은화: $playerSilver 개")
 }
 
-private fun placeOrder(menuData: String) {
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVENR_NAME.indexOf('\'')
     val tavenMaster = TAVENR_NAME.substring(0 until indexOfApostrophe)
-    println("마드리갈은 $tavenMaster 에게 주문합니다.")
+    println("${patronName}은 ${tavenMaster}에게 주문합니다.")
 
 //    val data = menuData.split(',')
 //    val type = data[0]
 //    val name = data[1]
 //    val price = data[2]
-    val (type, name, price) = menuData.split(',')
-   if (performPurchase(price.toDouble())){
-       val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
-       println(message)
 
-       val phrase = if (name =="Dragon's Breath"){
-           "마드리갈이 감탄한다 : ${toDragonSpeak("와 $name 진짜 좋구나!")}"
-       } else {
-           "마드리갈이 말한다: 감사합니다. $name"
-       }
-       println(phrase)
-   }
+    val (type, name, price) = menuData.split(',')
+//    if (performPurchase(price.toDouble())) {
+    val message = "${patronName}은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "${patronName}이 감탄한다 : ${toDragonSpeak("와 $name 진짜 좋구나!")}"
+    } else {
+        "${patronName}이 말한다: 감사합니다. $name"
+    }
+    println(phrase)
+//    }
 }
 
 private fun toDragonSpeak(phrase: String) =
--- src/hello.kt
+++ src/hello.kt
@@ -1,3 +1,42 @@
+import java.io.File
+
 fun main() {
-    println("안녕하세요?")
+//    val menuFile = File("menu-file.txt")
+//    menuFile.setReadable(true)
+//    menuFile.setWritable(true)
+//    menuFile.setExecutable(false)
+
+    // 동일한 코드사용 제거 .apply적용하는방법
+    val menuFile = File("menu-file.txt").apply {
+        setReadable(true)       // 암시적으로 menuFile.setReadable(true)가 된다.
+        setWritable(true)       // 암시적으로 menuFile.setWritable(true)가 된다.
+        setExecutable(false)    // 암시적으로 menuFile.setExecutable(false)가 된다.
+        // 람다 내부의 모든 함수 호출이 이제는 수신자에 관련되어 호출되므로
+        // 연관범위(relative scoping)또는 수신자에 대한 암시적 호출(implictly called) 라고 한다
+    }
+
+    val firstItemSquared = listOf(1,2,3).first().let {
+        it * it
+    }
+
+    //파일에 특정 문자열이 있는지 검사하는 코드
+    val servesDragonsBreath = menuFile.run {
+        readText().contains("Dragons's Breath")
+    }
+
+
+    fun nameIsLong(name: String) = name.length >= 20
+    fun playerCreateMessage(nameTooLong: Boolean): String {
+        return if (nameTooLong) {
+            "Name is too long. Please choose another name."
+        } else {
+            "Welcom, adventurer"
+        }
+    }
+
+    "Polarcubis, Supreme Master of NyeHack"
+        .run(::nameIsLong)
+        .run(::playerCreateMessage)
+        .run(::println)
+
 }
