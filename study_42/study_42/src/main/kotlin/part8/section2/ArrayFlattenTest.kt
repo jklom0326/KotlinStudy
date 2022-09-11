06c634e2829d6eded002878ceee6f9ee69a8928e
jklom0326 <jklom0326@gmail.com>
2022-09-11 23:59:49 +0900

    part8

--- study_42/study_42/src/main/kotlin/part8/section2/ArrayFlattenTest.kt
+++ study_42/study_42/src/main/kotlin/part8/section2/ArrayFlattenTest.kt
@@ -1,11 +1,12 @@
 package part8.section2
 
 fun main() {
-    val numbers = arrayOf(1,2,3)
-    val strs = arrayOf("one","two","three")
+    val numbers = arrayOf(1, 2, 3)
+    val strs = arrayOf("one", "two", "three")
     val simplArray = arrayOf(numbers, strs)
     simplArray.forEach { println(it) }
 
     val flattenSimpleArray = simplArray.flatten()
     println(flattenSimpleArray)
+
 }
